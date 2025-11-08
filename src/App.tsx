cfa0ba0018b72020b7b5099d4e14dae0cd1e5b0c
Jacek Pietrzyk <jacpie3k@gmail.com>
2025-11-08 21:20:39 +0100

    add some legal stuff

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,16 @@ import { Toaster } from "./components/ui/toaster";
 
 import Home from "./pages/home";
 import Checkout from "./pages/checkout";
+import {
+  PrivacyPage,
+  TermsPage,
+  CookiesPage,
+  ConsentsPage,
+  ReturnsPage,
+  ShippingPage,
+  SecurityPage,
+  ContactPage,
+} from "./pages/legalPages";
 
 function App() {
   return (
@@ -13,6 +23,16 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/checkout" element={<Checkout />} />
+
+            {/* Legal / compliance routes (PL) */}
+            <Route path="/privacy" element={<PrivacyPage />} />
+            <Route path="/terms" element={<TermsPage />} />
+            <Route path="/cookies" element={<CookiesPage />} />
+            <Route path="/consents" element={<ConsentsPage />} />
+            <Route path="/returns" element={<ReturnsPage />} />
+            <Route path="/shipping" element={<ShippingPage />} />
+            <Route path="/security" element={<SecurityPage />} />
+            <Route path="/contact" element={<ContactPage />} />
           </Routes>
         </div>
       </BrowserRouter>
