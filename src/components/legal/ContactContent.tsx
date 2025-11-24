9e0d0af20ffec1b75eb43f720d1afcfd23b0a9d8
Jacek Pietrzyk <jacpie3k@gmail.com>
2025-11-24 21:10:25 +0100

    Move legal content to md files

--- src/components/legal/ContactContent.tsx
+++ src/components/legal/ContactContent.tsx
@@ -1,14 +1,6 @@
+import ReactMarkdown from "react-markdown";
+import contactMd from "../../assets/legal/contact.md?raw";
+
 export function ContactContent() {
-  return (
-    <>
-      <p>
-        Masz pytania dotyczące zamówień, dostawy lub personalizacji? Skontaktuj
-        się z nami. Odpowiemy najszybciej jak to możliwe.
-      </p>
-      <ul>
-        <li>E-mail: kontakt@tuusimago.pl (przykładowy)</li>
-        <li>Godziny pracy: pn–pt 9:00–17:00</li>
-      </ul>
-    </>
-  );
+  return <ReactMarkdown>{contactMd}</ReactMarkdown>;
 }
